9937f0162c1d400a90f8e0305bcc8bd045c8c264
Romeo <53314439+Ro-off@users.noreply.github.com>
2019-10-07 21:44:25 +0300

    js added

---
+++ script.js
@@ -0,0 +1,7 @@
+var homejs = document.getElementById("homehtml");
+homejs.onmouseover = function() {
+  homejs.src = "https://img.icons8.com/ios/50/ffffff/home.png";
+};
+homejs.onmouseout = function() {
+  homejs.src = "https://img.icons8.com/ios-filled/50/ffffff/home.png";
+};
