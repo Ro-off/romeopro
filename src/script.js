ad283b18bb59b1349a061c2e8e5aec5665b2c055
Romeo <53314439+Ro-off@users.noreply.github.com>
2019-10-07 22:03:23 +0300

    test

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
-var homejs = document.getElementById("homehtml");
+/*var homejs = document.getElementById("homehtml");
 homejs.onmouseover = function() {
   homejs.src = "https://img.icons8.com/ios/50/ffffff/home.png";
 };
 homejs.onmouseout = function() {
   homejs.src = "https://img.icons8.com/ios-filled/50/ffffff/home.png";
 };
+*/
